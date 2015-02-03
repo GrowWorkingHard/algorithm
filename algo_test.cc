c1edc0efffd94eb81fca6d8469a1084618443523
francescoS <francesco.serafin.3@gmail.com>
2015-02-03 19:39:27 +0100

    Comments

--- algo_test.cc
+++ algo_test.cc
@@ -1,3 +1,23 @@
+//************************************************************************************//
+//*          - GrowWorkingHard'script - www.growworkinghard.wordpress.com -          *//
+//************************************************************************************//
+//                                                                                    //
+// This file is a script writed to complete our blog's articles (all the external     //
+// reference can be found there). It's needed to explain better concepts and give     //
+// to the users an example or a reference.                                            //
+// We don't give you the warranty that is the best solution, but we do all the        //
+// possible to hit it.                                                                //
+//                                                                                    //
+// You're invited to try our solution and communicate if something does't work or if  //
+// there is a better way to do it!                                                    //
+//                                                                                    //
+// Follow our blog: <http://www.growworkinghard.wordpress.com>                        //
+//                                                                                    //
+// Author: francescoS                                                                 //
+// Date: 2015-02-03                                                                   //
+//                                                                                    //
+//************************************************************************************//
+
 #include <stdlib.h>
 #include <stdio.h>  // printf command
 
