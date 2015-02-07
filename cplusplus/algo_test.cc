757fc6a75eeee0fe8b5f3300436d91deca4a9b96
francescoS <francesco.serafin.3@gmail.com>
2015-02-07 17:42:46 +0100

    These algorithms work also with char variables

--- cplusplus/algo_test.cc
+++ cplusplus/algo_test.cc
@@ -24,24 +24,24 @@ using namespace std;
 
 int main() {
 
-	vector<double> tmp_vec;
+	vector<char> tmp_vec;
 
-	tmp_vec.push_back(2.0);
-	tmp_vec.push_back(0.8);
-	tmp_vec.push_back(5.5);
-	tmp_vec.push_back(1.8);
-	tmp_vec.push_back(1.0);
-	tmp_vec.push_back(5.4);
-	tmp_vec.push_back(7.0);
+	tmp_vec.push_back('a');
+	tmp_vec.push_back('w');
+	tmp_vec.push_back('f');
+	tmp_vec.push_back('c');
+	tmp_vec.push_back('s');
+	tmp_vec.push_back('z');
+	tmp_vec.push_back('n');
 
-	double min_value = algo::Algorithm::min(tmp_vec);
-	printf("Min value: %f\n", min_value);
+	char min_value = algo::Algorithm::min(tmp_vec);
+	printf("Min value: %c\n", min_value);
 
 	algo::Algorithm::selection_sort(tmp_vec);
 
 	for (size_t i=0; i<tmp_vec.size(); i++) {
 
-		printf("Position %zu, value %f\n", i+1, tmp_vec[i]);
+		printf("Position %zu, value %c\n", i+1, tmp_vec[i]);
 
 	}
 
